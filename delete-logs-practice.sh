b42fc32c93e5699f0a4b39bad771eb3d9ee84b33
Somularam <reddysomularamalingeswar>
2023-12-19 10:46:16 +0530

    new file delete-logs-practice

--- delete-logs-practice.sh
+++ delete-logs-practice.sh
@@ -5,11 +5,11 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
-Source_Dir=$1
+Source_Dir="sample"
 
 if [ ! -d "$Source_Dir" ]
 then
-    echo -e "$RERROR: Source directory doesn't exist.$N"
+    echo -e "$R ERROR: Source directory doesn't exist.$N"
 fi
 
 File_to_delete=$(find "$Source_Dir" -type f -mtime +14 -name '*.logs')
