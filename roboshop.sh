757deea045f8338f56f44df3bcecd023de335912
Somularam <reddysomularamalingeswar>
2023-12-21 14:59:51 +0530

    roboshop.sh added to perform the project

--- roboshop.sh
+++ roboshop.sh
@@ -7,33 +7,36 @@ DOMAIN_NAME="lingaaws.tech"
 zoneid="Z0790319IEVF09XXXVM1"
 for i in ${INSTANCES[@]}
 do
+    echo "Instanlling $i"
     if [ $i == "mongodb" ] || [ $i == "redis" ] || [ $i == "mysql" ]
     then
-        INSTANCE_TYPE=t3.small
+        INSTANCE_TYPE="t3.small"
     else
-        INSTANCE_TYPE=t2.micro
+        INSTANCE_TYPE="t2.micro"
     fi
 
     PrivateIpAddress=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG --tag-specifications 'ResourceType=instance,Tags=[{Key=name,Value=$i}]' --query Instances[0].PrivateIpAddress --output text)
+    
+    echo "$i PublicIpAddress is : $PrivateIpAddress"
 
     aws route53 change-resource-record-sets \
-  --hosted-zone-id $zoneid \
-  --change-batch '
-  {
-    "Comment": "Creating a record set for cognito endpoint"
-    ,"Changes": [{
-      "Action"              : "CREATE"
-      ,"ResourceRecordSet"  : {
-        "Name"              : "'$i'.'$DOMAIN_NAME'"
-        ,"Type"             : "A"
-        ,"TTL"              : 1
-        ,"ResourceRecords"  : [{
-            "Value"         : "'$PrivateIpAddress'"
+    --hosted-zone-id $zoneid \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$PrivateIpAddress'"
+            }]
+        }
         }]
-      }
-    }]
-  }
-    '
+    }
+        '
 done
 
 
